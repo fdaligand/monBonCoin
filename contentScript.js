4f27b5ecbc0b682c0248eb534e7ee5c0df533c59
fdaligand <florent.daligand@gmail.com>
2016-07-02 18:25:59 +0200

    parse page and iter through each add of it

--- contentScript.js
+++ contentScript.js
@@ -1,19 +1,23 @@
 
 
-var buttonPressed = false;
-
-
 function modifyPage(msg){
 	console.log("message received")
-	console.log("mesage"+msg)
+
+	var ul = document.getElementsByClassName("tabsContent")[0].getElementsByTagName("ul")[0]
+	var items = ul.getElementsByTagName("li");
+	for (var i = 0; i <= items.length ; i++) {
+		
+		var addData = items[i].getElementsByTagName("a")[0].getAttribute("data-info");
+		var addInfo = JSON.parse(addData);
+		var addId = addInfo.ad_listid
+		console.log("add id:"+addId);
+		addHideButton(addId);
+	};
+	
 	document.body.style.border = "5px solid red";
-	buttonPressed = true;	
+		
 };
 
 chrome.runtime.onMessage.addListener(modifyPage);
-
-if (! buttonPressed ) {
-	document.body.style.border = "5px solid blue";
-}
 
 
